8f1cf6dbcea21294fef35d889f5b8c6aabe85c23
SlickyRussian <valery_biz@mail.ru>
2020-06-22 23:45:57 +0300

    Добавил файл с решенимем задачи №8.

---
+++ src/EighthTask.java
@@ -0,0 +1,9 @@
+public class EighthTask {
+    public static void main(String[] args) {
+        System.out.println();           //чтобы выделить результат
+        for (int i = 1; i < 51; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println();           //чтобы выделить результат
+    }
+}
